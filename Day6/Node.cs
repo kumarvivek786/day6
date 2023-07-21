ac5018524ea49105f5d58c55aab75132eca345d2
vivek <ikumarvivek786@gmail.com>
2023-07-21 11:38:41 +0530

    [Vivek]Custom Linked List]

---
+++ Day6/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    public class Node
+    {
+        public T data;
+        public Node<T> next;
+        public Node(T data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
---
+++ Day6/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Day6 Datastructure PracticeProblem");
+            Console.WriteLine("Select any one option for \n 1. Generic Custom Linklist");
+            int operation = Convert.ToInt32(Console.ReadLine());
+            switch (operation)
+            {
+                case 1:
+                    CustomeGenericLinkList<int> integerLinkList = new CustomeGenericLinkList<int>();
+                    integerLinkList.AddLast(10);
+                    integerLinkList.AddLast(20);
+                    integerLinkList.Addfirst(30);
+                    integerLinkList.Addfirst(40);
+                    integerLinkList.DisplayList();
+
+                    CustomeGenericLinkList<string> stringLinkList = new CustomeGenericLinkList<string>();
+                    stringLinkList.AddLast("mahesh");
+                    stringLinkList.Addfirst("Gangurde");
+                    stringLinkList.DisplayList();
+                    Console.WriteLine("after removing Element");
+                    stringLinkList.Remove("Gangurde");
+                    stringLinkList.DisplayList();
+                    break;
+                default:
+                    Console.WriteLine("Please Enter Valid Option");
+                    break;
+            }
+        }
+    }
+}
